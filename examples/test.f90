5c3d4b079673d41bb2117bab5431d4b64179ef22
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2021-04-02 11:29:31 -0300

    fix(examples): Move test to examples folder. Insert free function.

--- examples/test.f90
+++ examples/test.f90
@@ -17,19 +17,26 @@ program test
     write(*,*)'Call init ...'
     call D%init()
     write(*,*)'Check if dictionary exists: ',D%exists()
-
+    write(*,*)
     write(*,*)'Check if dictionary is empty: ',D%empty()
     write(*,*)'Add a vector to it ...'
     f = [1.d0,5.d0,3.d0]
     call D%add('mytag',f)
     write(*,*)'Check if dictionary is empty: ',D%empty()
-
+    write(*,*)
     write(*,*)'Retrieve vector from map ...'
     write(*,*)D%get('mytag')
-
+    write(*,*)
     write(*,*)'Change the vector in Fortran ...'
     f(2) = 15.d0
-    write(*,*)'Retrieve vector from map ...'
+    write(*,*)'Retrieve same vector from map ...'
     write(*,*)D%get('mytag')
+    write(*,*)
+    write(*,*)'Deallocating dictionary ...'
+    call D%destroy()
+    write(*,*)'Check if dictionary exists: ',D%exists()
+
+
+
 
 end program test
